c04a8a0781c7663939acd2284f9d68a023fd0518
Brian Speelman <67787940+BrianStatesThat@users.noreply.github.com>
2025-02-16 11:37:11 +0000

    Components and NPM Stuff

---
+++ myportfolio/components/Contact.jsx
@@ -0,0 +1,35 @@
+import { assets } from '@/assets/assets'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat
+    bg-center bg-[length:90%_auto]'>
+        <h4 className='text-center mb-2 text-lg font-Ovo'>Let's Connect</h4>
+        <h2 className='text-center text-5xl font-Ovo'>Send me a message</h2>
+
+        <p className='text-center max-w-2x mx-auto mt-5 mb-12 font-Ovo'>
+        Share your name, email, and a little about your project or question.
+         I would love to hear from you! Once you are ready, click Submit, and I will be in touch soon to help however I can. Looking forward to connecting!
+        </p>
+
+        <form className='max-w-2xl mx-auto'>
+          <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
+            <input className='flex-1 p-3 outline-none border-[0.5px] border-gray-400
+            rounded-md bg-white' type="text" placeholder='Please enter your name' required />
+            <input className='flex-1 p-3 outline-none border-[0.5px] border-gray-400
+            rounded-md bg-white' type="email" placeholder='Please enter your email address' required />
+          </div>
+          <textarea className='w-full mb-6 p-4 outline-none border-[0.5px] border-gray-400
+            rounded-md bg-white' rows='6' placeholder='Please enter your message' name="" id="" required ></textarea>
+
+          <button type='submit'
+            className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80
+            text-white rounded-full mx-auto hover:bg-black duration-500' >Submit now <Image
+            src={assets.right_arrow_white                     }          />     </button>
+        </form>
+    </div>
+  )
+}
+
+export default Contact
---
+++ myportfolio/components/Header.jsx
@@ -0,0 +1,36 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-11/12 max-w-3xl text-center mx-auto 
+    h-screen flex flex-col items-center justify-center gap-4'>
+      <div>
+      <Image src={assets.profile_img} alt=''
+      className='rounded-full w-32'/>
+      </div>
+      <h3 className='text-3xl sm:text-6xl lg:text-[66px] font-O' >
+        Hello! I'm Mandilakhe Brian Speelman
+      </h3>
+      <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
+        Front-End Web Developer based in Gqeberha
+      </h1>
+      <p className='max-w-2xl mx-auto font-Ovo'>
+      Hi, I'm Mandilake Brian Speelman, a front end developer and aspiring AI engineer with expertise in IT support, 
+      cybersecurity, and system administration.
+      I've built successful landing pages, completed multiple certifications, and contribute to open-source projects,
+      driven by a passion for creating user-focused solutions.
+      </p>
+      <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+        <a href="/sample-resume.pdf" download 
+        className='px-10 py-3 border rounded-full border-gray-500
+        flex items-center gap-2'>
+          my resume <Image src={assets.download_icon} alt='' className='w-4'/></a>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Header;
