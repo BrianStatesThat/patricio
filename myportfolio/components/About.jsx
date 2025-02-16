33426edb806dcc6c877c01a114ff73de4cd33964
Brian Speelman <67787940+BrianStatesThat@users.noreply.github.com>
2025-02-16 13:18:15 +0000

    Margins and padding

--- myportfolio/components/About.jsx
+++ myportfolio/components/About.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20' >
-        <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
+    <div id='about' className='w-full px-[12%] py-20 scroll-mt-28 ' >
+        <h4 className='text-center mb-4 text-lg font-Ovo'>Introduction</h4>
         <h2 className='text-center text-5xl font-Ovo'>About me</h2>
 
         <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
