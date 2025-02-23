dc4337078accb16f1c0158a31a3434ac4b1c5f10
Brian Speelman <67787940+BrianStatesThat@users.noreply.github.com>
2025-02-23 09:22:36 +0000

    Overflow bug fixed.

--- myportfolio/components/Contact.jsx
+++ myportfolio/components/Contact.jsx
@@ -29,9 +29,9 @@ const Contact = () => {
   };
 
   return (
-    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20'>
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 overflow-x-hidden'>
         <h4 className='text-center mb-2 text-lg font-Ovo'>Let's Connect</h4>
-        <h2 className='text-center text-5xl font-Ovo'>Send me a message</h2>
+        <h2 className='text-center text-5xl font-Ovo '>Send me a message</h2>
 
         <p className='text-center max-w-2x mx-auto mt-5 mb-12 font-Ovo'>
         Share your name, email, and a little about your project or question.
@@ -39,12 +39,12 @@ const Contact = () => {
         </p>
 
         <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
-          <div className='flex flex-row justify-between gap-6 mt-10 mb-8'>
-            <input className='flex-1 p-3 outline-none border-[0.5px] border-gray-400
-            rounded-md bg-white' type="text" placeholder='Please enter your name' required
+          <div className='flex flex-col justify-between gap-6 mt-10 mb-8 overflow-hidden'>
+            <input className=' p-3 outline-none border-[0.5px] border-gray-400
+            rounded-md bg-white w-full' type="text" placeholder='Please enter your name' required
             name='name' />
-            <input className='flex-1 p-3 outline-none border-[0.5px] border-gray-400
-            rounded-md bg-white' type="email" placeholder='Please enter your email address' required 
+            <input className=' p-3 outline-none border-[0.5px] border-gray-400
+            rounded-md bg-white w-full' type="email" placeholder='Please enter your email address' required 
             name='email'/>
           </div>
           <textarea className='w-full mb-6 p-4 outline-none border-[0.5px] border-gray-400
