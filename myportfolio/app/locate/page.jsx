e597fb8586d7d54491c04e7411395d10a0f404cd
brianstatesthat <mbspeelman@gmail.com>
2025-03-25 03:29:57 +0200

    allowed accelerometer in iframe

--- myportfolio/app/locate/page.jsx
+++ myportfolio/app/locate/page.jsx
@@ -7,7 +7,8 @@ const Page = () => {
         <div className='flex flex-col w-full h-full pt-[64] pb-[84px] px-1 item-center justify-center overflow-x-hidden'>
             <h2 className='text-center pb-[5px]'>Locate Us</h2>
             <div id="map" className='h-fit w-[350x] rounded-lg mx-auto md:w-[450px] lg:w-[600px] xl:w-[800px] overflow-hidden'>
-            <iframe src="https://www.google.com/maps/embed?pb=!4v1742298981461!6m8!1m7!1sAvWHD6BwJ2kuFf3c5RaXJQ!2m2!1d-33.87291611363754!2d25.58397673796407!3f28.542775740084945!4f-5.703038083750528!5f1.2205863979737528" className='w-full h-[450px] rounded-sm mx-auto' referrerPolicy="no-referrer-when-downgrade">
+            <iframe src="https://www.google.com/maps/embed?pb=!4v1742298981461!6m8!1m7!1sAvWHD6BwJ2kuFf3c5RaXJQ!2m2!1d-33.87291611363754!2d25.58397673796407!3f28.542775740084945!4f-5.703038083750528!5f1.2205863979737528"
+            allow="accelerometer; gyroscope" className='w-full h-[450px] rounded-sm mx-auto' referrerPolicy="no-referrer-when-downgrade">
             </iframe>
             </div>
         </div>
