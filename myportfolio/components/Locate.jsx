991478533edf976c5d5604a35f949d872f2494dc
brianstatesthat <mbspeelman@gmail.com>
2025-03-25 23:18:29 +0200

    minor bug fix

--- myportfolio/components/Locate.jsx
+++ myportfolio/components/Locate.jsx
@@ -10,7 +10,7 @@ const Locate = () => {
             Find us at Patrick's Barber Shop in the heart of Boxongo! We're here to keep you looking sharp and stylish.
              Conveniently located for all your grooming needs - swing by and let us take care of your fresh cut today!
         </p>
-            <div id="map" className='h-fit w-[300x] rounded-lg mx-auto md:w-[450px] lg:w-[600px] xl:w-[800px] overflow-hidden'>
+            <div id="map" className='h-fit w-[300x] rounded-lg mx-auto md:w-[450px] lg:w-[600px] xl:w-[800px] overflow-x-hidden'>
             <iframe src="https://www.google.com/maps/embed?pb=!4v1742298981461!6m8!1m7!1sAvWHD6BwJ2kuFf3c5RaXJQ!2m2!1d-33.87291611363754!2d25.58397673796407!3f28.542775740084945!4f-5.703038083750528!5f1.2205863979737528"
             allow="accelerometer; gyroscope" className='w-full h-[350px] rounded-sm mx-auto' referrerPolicy="no-referrer-when-downgrade">
             </iframe>
