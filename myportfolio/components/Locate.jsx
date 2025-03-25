eb1909306d6188eccfad827cd27dce63e90fcbc5
brianstatesthat <mbspeelman@gmail.com>
2025-03-25 23:24:12 +0200

    minor bug fix3

--- myportfolio/components/Locate.jsx
+++ myportfolio/components/Locate.jsx
@@ -9,7 +9,7 @@ const Locate = () => {
             
             <div id="map" className='h-fit w-[300x] rounded-lg mx-auto md:w-[450px] lg:w-[600px] xl:w-[800px] overflow-x-hidden'>
             <iframe src="https://www.google.com/maps/embed?pb=!4v1742298981461!6m8!1m7!1sAvWHD6BwJ2kuFf3c5RaXJQ!2m2!1d-33.87291611363754!2d25.58397673796407!3f28.542775740084945!4f-5.703038083750528!5f1.2205863979737528"
-            allow="accelerometer; gyroscope" className='w-full h-[350px] rounded-sm mx-auto' referrerPolicy="no-referrer-when-downgrade">
+            allow="accelerometer; gyroscope" className='relative pt-[128px] w-full h-[350px] rounded-sm mx-auto' referrerPolicy="no-referrer-when-downgrade">
             </iframe>
             </div>
             <p className='text-center max-w-2x mx-auto mt-5 mb-12 font-Ovo px-1'>
